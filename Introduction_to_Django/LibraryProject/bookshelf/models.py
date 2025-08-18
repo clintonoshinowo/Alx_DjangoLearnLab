3d3c8c63304fd0e7ee8aab0fbdc8161fba52cf0d
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-08-18 14:24:35 -0600

    update

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.Charfield(max_length=200)
-    author = models.Charfield(max_length=100)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     title = models.IntegerField()
 
     def__str__(self):
