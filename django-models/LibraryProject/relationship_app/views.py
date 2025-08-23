5678d1eca9fd7664c9391b9a10359d10a89b9e1e
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-08-23 10:37:21 -0600

    "Fix: Log user in after registration"

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 # LibraryProject/relationship_app/views.py
 
 from django.shortcuts import render, redirect
+from django.contrib.auth import login # NEW IMPORT
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
@@ -20,8 +21,9 @@ def register(request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
-            # Redirect to the login page after a successful registration.
-            return redirect(reverse_lazy("relationship_app:login"))
+            # Log the user in and redirect to the home page.
+            login(request, new_user) # NEW LINE
+            return redirect(reverse_lazy("relationship_app:index"))
 
     # Display a blank or invalid form.
     context = {"form": form}
