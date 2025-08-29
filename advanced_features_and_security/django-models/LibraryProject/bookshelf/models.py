978765baf894c93a3c8769623825d66c98a8239a
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-08-29 15:27:19 -0600

    new

--- advanced_features_and_security/django-models/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/django-models/LibraryProject/bookshelf/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def _str_(self):
+        return self.username
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
