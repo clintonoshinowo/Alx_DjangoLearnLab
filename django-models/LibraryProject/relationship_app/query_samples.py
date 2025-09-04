0c08aa0fc6aa08bfe6ed78a344ebadc3362f0ed1
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-09-04 07:54:09 -0600

    changes to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,7 +6,7 @@
 query_for_all_books = ["books.all()"],["Library.objects.get(name=library_name)"]
 
 # Example of a query to get a specific author
-query_for_author = "Author.objects.get(name='J.R.R. Tolkien')"
+query_for_author = ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
 
 # Example of a query to find books published in a certain year
 query_for_year = "Book.objects.filter(publication_date__year=2023)"
