ada7b9decf0fe507bf14d939c8a2967fe7dcacea
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-09-06 10:16:22 -0600

    "Fixed Django admin error and created CustomUser model migration"

--- advanced_features_and_security/admin.py
+++ advanced_features_and_security/admin.py
@@ -1,37 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
+# Import your new CustomUser model
 from .models import CustomUser
 
-class CustomUserAdmin(BaseUserAdmin):
-    """
-    A custom Admin class for the CustomUser model.
-    It adds the date_of_birth and profile_photo fields to the user
-    admin page.
-    """
-    # The list_display will show these fields in the user list view
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
-
-    # The fieldsets will organize the fields in the user detail form
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-    
-    # Make sure to include these fields in the add form as well
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password', 'password2'),
-        }),
-    )
-    
-    # Set the ordering of the users
-    ordering = ('email',)
-
-# Register the custom user model with the new admin class
-admin.site.register(CustomUser, CustomUserAdmin)
-
-# Register your models here.
+# Register your CustomUser model with the admin site.
+# We'll use a class that inherits from Django's built-in UserAdmin
+# to customize the display.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # This list defines the columns to display in the admin list view.
+    # We are using fields that actually exist on the CustomUser model
+    # (inherited from AbstractUser).
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined')
--- advanced_features_and_security/models.py
+++ advanced_features_and_security/models.py
@@ -1,66 +1,110 @@
-# advanced_features_and_security/models.py
-
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from django.conf import settings
+from datetime import date
+import uuid
 
-class CustomUserManager(BaseUserManager):
-    """
-    Custom manager for the CustomUser model.
-    It overrides the default user and superuser creation methods
-    to ensure the new fields are handled correctly.
-    """
-    def create_user(self, email, password=None, **extra_fields):
-        """
-        Creates and saves a regular user with the given email and password.
-        The email is set as the username to ensure uniqueness.
-        """
-        if not email:
-            raise ValueError(_('The Email field must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# Import the AbstractUser class
+from django.contrib.auth.models import AbstractUser
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        """
-        Creates and saves a superuser with the given email and password.
-        """
-        # Ensure superusers have the necessary permissions
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-            
-        return self.create_user(email, password, **extra_fields)
-
+# IMPORTANT: This is the new custom user model you need to define.
 class CustomUser(AbstractUser):
-    """
-    A custom user model that extends Django's AbstractUser.
-    We are replacing the default username field with email for login,
-    and adding a date of birth and a profile photo.
-    """
-    # Overriding the default username field to use email for authentication
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
-    
-    # Adding custom fields
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    # Add any extra fields you want to your user model here,
+    # or just leave it empty for now if you don't need any.
+    pass
 
-    # Use the custom manager for this model
-    objects = CustomUserManager()
 
-    # Set the email field as the unique identifier for authentication
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] # No extra required fields at creation for superuser
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
-        return self.email
+        return self.name
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter the book\'s natural language (e.g. English, French, Japanese etc.)')
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+    
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    publication_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+    # The foreign key to your new custom user model
+    borrower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    
+    class Meta:
+        ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
