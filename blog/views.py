6e1517dc1cb476c25da75a71763dd48736abf823
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-09-09 11:02:49 -0600

    added new line to the blog views.py

--- blog/views.py
+++ blog/views.py
@@ -1,70 +1,76 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView, 
-    DetailView, 
-    CreateView, 
-    UpdateView, 
-    DeleteView
-)
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.decorators import login_required
 
-# Simple function-based view for the index page.
-def index(request):
-    return render(request, 'blog/index.html')
+# Create your views here.
 
-# Class-based view to display a list of all posts.
 class PostListView(ListView):
+    """
+    A view to display a list of blog posts.
+    It uses the Post model and orders the posts by creation date in descending order.
+    The template name is set to 'blog/post_list.html'.
+    """
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
-    paginate_by = 10
+    ordering = ['-date_created']
 
-# Class-based view to display a single post in detail.
 class PostDetailView(DetailView):
+    """
+    A view to display the details of a single blog post.
+    It uses the Post model and the template name is 'blog/post_detail.html'.
+    """
     model = Post
     template_name = 'blog/post_detail.html'
 
-# Class-based view to create a new post.
-# It requires the user to be logged in and redirects to the new post's detail page on success.
 class PostCreateView(LoginRequiredMixin, CreateView):
+    """
+    A view to create a new blog post.
+    It requires the user to be logged in to access the view.
+    It uses the Post model and the template name is 'blog/post_form.html'.
+    The fields 'title', 'content', and 'author' are included in the form.
+    Upon successful form submission, it redirects to the post list view.
+    """
     model = Post
-    fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    fields = ['title', 'content', 'author']
+    success_url = reverse_lazy('blog:blog_index')
 
-# Class-based view to update an existing post.
-# It requires the user to be logged in and must be the author of the post.
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    A view to update an existing blog post.
+    It requires the user to be logged in and to be the author of the post to update it.
+    It uses the Post model and the template name is 'blog/post_form.html'.
+    The fields 'title', 'content', and 'author' are included in the form.
+    Upon successful form submission, it redirects to the post detail view.
+    """
     model = Post
-    fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    fields = ['title', 'content', 'author']
     
     def test_func(self):
+        """
+        Check if the logged-in user is the author of the post.
+        """
         post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        return self.request.user == post.author
 
-# Class-based view to delete a post.
-# It requires the user to be logged in and must be the author of the post.
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    A view to delete an existing blog post.
+    It requires the user to be logged in and to be the author of the post to delete it.
+    It uses the Post model and the template name is 'blog/post_confirm_delete.html'.
+    Upon successful form submission, it redirects to the post list view.
+    """
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('blog-home')
+    success_url = reverse_lazy('blog:blog_index')
     
     def test_func(self):
+        """
+        Check if the logged-in user is the author of the post.
+        """
         post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        return self.request.user == post.author
