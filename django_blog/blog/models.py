e100dfd3c9e2fcfe4dc2bd6b926a5403d76bfdc5
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-09-10 11:04:54 -0600

    changes to blog/models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,21 +1,43 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.urls import reverse
 
 class Post(models.Model):
     """
     Represents a blog post.
     """
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
-        """
-        Returns a string representation of the post.
-        """
+        """String representation of the Post model."""
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    """
+    Represents a comment on a blog post.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        """String representation of the Comment model."""
+        return f'Comment by {self.author} on {self.post}'
